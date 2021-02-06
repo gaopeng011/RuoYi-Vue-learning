284100ea21d6bf7b477bdad1a83a4a6eaf8949a2
gaopeng <akgaopeng@163.com>
2021-02-06 22:36:07 +0800

    ruoyi-admin模块拆分成 ruoyi-admin，ruoyi-run，ruoyi-monitor三个模块

--- bin/run.bat
+++ bin/run.bat
@@ -4,11 +4,11 @@ echo [信息] 运行Web工程。
 echo.
 
 cd %~dp0
-cd ../ruoyi-admin/target
+cd ../ruoyi-run/target
 
 set JAVA_OPTS=-Xms256m -Xmx1024m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m
 
-java -jar %JAVA_OPTS% ruoyi-admin.jar
+java -jar %JAVA_OPTS% ruoyi-run.jar
 
 cd bin
 pause
